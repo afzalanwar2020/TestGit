c894c28f925ef85f6d33605627e0e5eea1ea7251
aafzalan <afzal-anwar.afzal-anwar@capgemini.com>
2022-11-16 11:23:53 +0530

    Commit Tester1 for GIt

--- src/Test1.java
+++ src/Test1.java
@@ -3,6 +3,7 @@ public class Test1 {
         public static void main(String[] args) {
             System.out.println("Hello from Tester1....");
             System.out.println("Hello from Tetser1 just for Test....");
+            System.out.println("Hello from Tetser1 just for Test....");
 
         }
 
