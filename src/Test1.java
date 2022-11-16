11aef4873be11d6869c270dfab1c768d96a511f8
aafzalan <afzal-anwar.afzal-anwar@capgemini.com>
2022-11-16 11:15:50 +0530

    Commit final for main for GIt

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,9 @@
 public class Test1 {
 
         public static void main(String[] args) {
-            System.out.println("Hello Test1....1");
+            System.out.println("Hello from Master....1");
+            System.out.println("Hello from Master just for test....1");
+
         }
 
 }
